80ad23296a8a13faee58e3b401ce33b064deb516
Karen Calderon <krcatari@umsa.bo>
2026-06-10 11:08:38 -0400

    implementar InventoryController para validación de stock y operaciones CRUD

---
+++ src/main/java/com/ecommerce/inventory_service/controller/InventoryController.java
@@ -0,0 +1,53 @@
+package com.ecommerce.inventory_service.controller;
+
+import com.ecommerce.inventory_service.dto.InventoryRequestDTO;
+import com.ecommerce.inventory_service.dto.InventoryResponseDTO;
+import com.ecommerce.inventory_service.service.InventoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    // --- Endpoint (LOgica de Negocio) ---
+    @GetMapping("/{sku}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku") String sku, @RequestParam("quantity") Integer quantity) {
+        return inventoryService.isInStock(sku, quantity);
+    }
+
+    // --- Endpoints CRUD  ---
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public InventoryResponseDTO createInventory(@RequestBody @Valid InventoryRequestDTO InventoryRequestDTO) {
+        return inventoryService.createInventory(InventoryRequestDTO);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponseDTO> getAllInventory() {
+        return inventoryService.getAllInventory();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public InventoryResponseDTO updateInventory(@PathVariable Long id,
+                                             @RequestBody @Valid InventoryRequestDTO InventoryRequestDTODTODTO) {
+        return inventoryService.updateInventory(id, InventoryRequestDTODTODTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteInventory(@PathVariable Long id) {
+        inventoryService.deleteInventory(id);
+    }
+
+}
